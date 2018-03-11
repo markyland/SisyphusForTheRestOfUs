7935b1c188db5ef194e7a5604f291309b39ff3bf
Mark Zeldis <mzeldis@walleyetrading.net>
2018-03-11 13:09:30 -0400

    added in/out files

--- src/SisyphusForTheRestOfUs.java
+++ src/SisyphusForTheRestOfUs.java
@@ -1,6 +1,7 @@
 import javax.imageio.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
 import java.util.*;
@@ -14,9 +15,59 @@ import java.util.*;
 public class SisyphusForTheRestOfUs {
 
     public SisyphusForTheRestOfUs(){
-        JPanel panel = new JPanel();
+        JPanel panel = new JPanel(new BorderLayout());
 
+        panel.add(new JLabel(new ImageIcon(getClass().getResource("header.png"))), BorderLayout.NORTH);
+        panel.add(createMainPanel(), BorderLayout.CENTER);
+        panel.add(createButtonPanel(), BorderLayout.SOUTH);
 
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private JPanel createMainPanel(){
+        final JTextField vectorFileInputTF = new JTextField(40);
+
+        JButton vectorFileInputButton = new JButton(new AbstractAction("..."){
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fc = new JFileChooser();
+
+                int returnVal=fc.showOpenDialog(SwingUtilities.getWindowAncestor(vectorFileInputTF));
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    vectorFileInputTF.setText(fc.getSelectedFile().getPath());
+                }
+            }
+        });
+
+        JPanel p = new JPanel(new BorderLayout(10, 10));
+        p.add(new JLabel("Ascii Vector File : "), BorderLayout.WEST);
+        p.add(vectorFileInputTF, BorderLayout.CENTER);
+        p.add(vectorFileInputButton, BorderLayout.EAST);
+        p.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        return p;
+    }
+
+    private JPanel createButtonPanel(){
+        JButton convertButton = new JButton("Convert!");
+
+        JButton exitButton = new JButton(new AbstractAction("Exit"){
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        JPanel p = new JPanel();
+        p.add(convertButton);
+        p.add(exitButton);
+
+        return p;
     }
 
     public static void main(String args[]){
