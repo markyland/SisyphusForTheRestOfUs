2f1ea165551f3de4cc1e591aeae7b562c8913e52
Mark Zeldis <mzeldis@walleyetrading.net>
2018-04-15 18:15:33 -0400

    testing some stuff

--- src/TestErase.java
+++ src/TestErase.java
@@ -26,6 +26,7 @@ public class TestErase {
 
                 eraseToManually(dc, com.slightlyloony.jsisyphus.Point.fromRT(1, 0), true);
 
+                eraseAndCircle(dc, .65, 330, .15);
                 eraseAndCircle(dc, .65, 300, .15);
                 eraseAndCircle(dc, .65, 270, .15);
                 eraseAndCircle(dc, .65, 240, .15);
@@ -35,10 +36,38 @@ public class TestErase {
                 eraseAndCircle(dc, .65, 120, .15);
                 eraseAndCircle(dc, .65, 90, .15);
                 eraseAndCircle(dc, .65, 60, .15);
+                eraseAndCircle(dc, .65, 30, .15);
+                eraseAndCircle(dc, .65, 0, .15);
+//
+                eraseAndCircle(dc, .35, 330, .1);
+                eraseAndCircle(dc, .35, 300, .1);
+                eraseAndCircle(dc, .35, 270, .1);
+                eraseAndCircle(dc, .35, 240, .1);
+                eraseAndCircle(dc, .35, 210, .1);
+                eraseAndCircle(dc, .35, 180, .1);
+                eraseAndCircle(dc, .35, 150, .1);
+                eraseAndCircle(dc, .35, 120, .1);
+                eraseAndCircle(dc, .35, 90, .1);
+                eraseAndCircle(dc, .35, 60, .1);
+                eraseAndCircle(dc, .35, 30, .1);
+                eraseAndCircle(dc, .35, 0, .1);
 
-                eraseAndCircle(dc, .4, 0, .1);
-                eraseAndCircle(dc, .3, 0, .2);
-                eraseAndCircle(dc, .2, 0, .3);
+                eraseAndCircle(dc, .15, 330, .05);
+                eraseAndCircle(dc, .15, 300, .05);
+                eraseAndCircle(dc, .15, 270, .05);
+                eraseAndCircle(dc, .15, 240, .05);
+                eraseAndCircle(dc, .15, 210, .05);
+                eraseAndCircle(dc, .15, 180, .05);
+                eraseAndCircle(dc, .15, 150, .05);
+                eraseAndCircle(dc, .15, 120, .05);
+                eraseAndCircle(dc, .15, 90, .05);
+                eraseAndCircle(dc, .15, 60, .05);
+                eraseAndCircle(dc, .15, 30, .05);
+                eraseAndCircle(dc, .15, 0, .05);
+
+//                eraseAndCircle(dc, .4, 0, .1);
+//                eraseAndCircle(dc, .3, 0, .2);
+//                eraseAndCircle(dc, .2, 0, .3);
 
 //                eraseAndCircle(dc, .4, 0, .1);
 //                eraseAndCircle(dc, .39, 90, .1);
@@ -81,12 +110,28 @@ public class TestErase {
 
         System.err.println(isOut);
 
-        while ((isOut ? (rho<=point.rho && rho<=1) : (rho>=point.rho && rho>=0)) || (Math.abs(theta-point.theta)%PI2)>(.015 / rho)) {
-            theta = theta + (isOut ? 1 : -1) * (.015 / rho);
+        double turnAmount=.1;
+
+        while ((isOut ? (rho<=point.rho && rho<=1) : (rho>=point.rho && rho>=0))) {
+            theta = theta + (isOut ? 1 : -1) * turnAmount;
 
             rho = theta / (2 * Math.PI) * eraseSpace;
 
             dc.lineTo(dc.getCurrentRelativePosition().vectorTo(com.slightlyloony.jsisyphus.Point.fromRT(rho, theta)));
+        }
+
+        double thetaLeft=theta%PI2 > point.theta ? theta%PI2-point.theta : theta%PI2-point.theta+PI2;
+        thetaLeft=(int)(thetaLeft/turnAmount)*turnAmount;
+
+        while (thetaLeft>0) {
+            theta = theta + (isOut ? 1 : -1) * turnAmount;
+
+            rho = theta / (2 * Math.PI) * eraseSpace;
+
+            dc.lineTo(dc.getCurrentRelativePosition().vectorTo(com.slightlyloony.jsisyphus.Point.fromRT(rho, theta)));
+
+            thetaLeft=theta%PI2 > point.theta ? theta%PI2-point.theta : theta%PI2-point.theta+PI2;
+            thetaLeft=(int)(thetaLeft/turnAmount)*.1;
         }
 
         theta=(int)(theta/PI2)*PI2  + point.theta;
