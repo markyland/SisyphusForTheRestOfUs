8d70f3aac6d1f249df4d0f297504c3d476cf153d
Omar Elamri <ea_do@icloud.com>
2020-08-09 18:32:06 -0500

    fix .equals 🤦‍♂️

--- src/v1/SFTROUCLI.java
+++ src/v1/SFTROUCLI.java
@@ -5,7 +5,7 @@ import javax.swing.JProgressBar;
 public class SFTROUCLI {
     public static void main(String[] args) {
         try {
-            GraphSolver.convert(args[0], args[1] + ".thr", args[1] + ".png", args[2] == "true", new JProgressBar(0, 100));
+            GraphSolver.convert(args[0], args[1] + ".thr", args[1] + ".png", args[2].equals("true"), new JProgressBar(0, 100));
         } catch (Throwable exception) {
             System.out.println(exception.getMessage());
         }
