d291db5b8f21eaa1f4c94983050708325c1903ca
Omar Elamri <ea_do@icloud.com>
2020-08-21 18:09:13 -0500

    fix cli

--- src/v1/SFTROUCLI.java
+++ src/v1/SFTROUCLI.java
@@ -7,7 +7,8 @@ public class SFTROUCLI {
         try {
             GraphSolver.convert(args[0], args[1] + ".thr", args[1] + ".png", args[2].equals("true"), new JProgressBar(0, 100));
         } catch (Throwable exception) {
-            System.out.println(exception.getMessage());
+            System.out.println("Error: " + exception.getMessage());
+            System.exit(-1);
         }
     }
 }
